ef35b91b35634e9b90dd81dc122a3bc2c711c6b0
Brayan0916 <brayan0916@hotmail.com>
2019-08-08 09:29:14 -0500

    Implementacion de la Pagina Index Parte 1

--- src/java/Configuraciones/Conexion.java
+++ src/java/Configuraciones/Conexion.java
@@ -5,10 +5,36 @@
  */
 package Configuraciones;
 
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author pc
  */
 public class Conexion {
+      private final String base = "datos2";
+    private final String user = "root";
+    private final String password = "";
+    private final String url = "jdbc:mysql://localhost/bdcarritocompras";
+    private Connection con = null;
     
+    public Connection getConexion()
+    {
+      
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con = (Connection) DriverManager.getConnection(this.url, this.user, this.password);
+            
+        } catch(SQLException e)
+        {
+            System.err.println(e);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+      return con;  
+    }
 }
